47fd4873cc19d01a0c256546bd3ef99f81765b5a
curtindoavida <202490+curtindoavida@users.noreply.github.com>
2026-02-20 12:17:48 -0300

    ok

--- main.js
+++ main.js
@@ -85,9 +85,6 @@ document.addEventListener('DOMContentLoaded', () => {
             try {
                 const response = await fetch('https://sociap.io/api/contact', {
                     method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
                     body: JSON.stringify(payload)
                 });
 
